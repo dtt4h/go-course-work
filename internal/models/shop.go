e81d8472f540b814167845b86b5bb03122491121
dtt4h <dtt4h322@gmail.com>
2025-10-16 12:25:48 +0700

    feat: init shop model

--- internal/models/shop.go
+++ internal/models/shop.go
@@ -1,1 +1,15 @@
 package models
+
+type Shop struct {
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	Address        string `json:"address"`
+	Phone          string `json:"phone"`
+	Specialization string `json:"specialization"`
+}
+
+type ShopSummary struct {
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	Specialization string `json:"specialization"`
+}
